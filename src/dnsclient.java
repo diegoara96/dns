98c6610a7a2d8f9123bcaedacc7f95ff8345fa5f
diego <diego@diego-PC>
2018-04-03 20:43:44 +0200

    comienza lo bueno

---
+++ src/dnsclient.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import es.uvigo.det.ro.simpledns.*;
+
+public class dnsclient {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println(args[0]);
+		BufferedReader algo = new BufferedReader(new InputStreamReader(System.in));
+		String dominio=algo.readLine();
+		
+		do {
+			
+		System.out.println(dominio);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}while((dominio=algo.readLine())!= null);
+		
+		
+	}
+
+}
