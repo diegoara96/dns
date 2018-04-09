f9e40a83c51985bf2e9c813841422162bcae9969
diego <diego@diego-PC>
2018-04-09 21:33:35 +0200

    empieza a ir

--- src/dnsclient.java
+++ src/dnsclient.java
@@ -1,49 +1,132 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.ArrayList;
 
 import es.uvigo.det.ro.simpledns.*;
+import funciones.EnvioPaquetes;
 
 public class dnsclient {
 
-	public static void main(String[] args) throws IOException {
-		if(args.length!=2) {
+	public static void main(String[] args) throws Exception {
+		String modoConexion;
+		if (args.length != 2) {
 			System.out.println("numero de argumentos incorrecto");
 		}
-		args[0]=args[0].toLowerCase();
+		args[0] = args[0].toLowerCase();
 		System.out.println(args[0]);
-		if(!(args[0].equals("-t")||args[0].equals("-u"))) {
-			 System.out.println("modo de conexion distinto de -(t|u)");
+		if (!(args[0].equals("-t") || args[0].equals("-u"))) {
+			System.out.println("modo de conexion distinto de -(t|u)");
 		}
-		 if (args[0].equals("-t")){
+		if (args[0].equals("-t")) {
+			modoConexion = "UDP";
+
 			System.out.println("No se halla implementado el uso de TCP. Se usará UDP");
-			args[0]="-u";
+			args[0] = "-u";
+		} else
+			modoConexion = "UDP";
+		
+		String ipCachos[] = args[1].split("\\.");
+		byte ip[] = new byte[4];
+		
+		if(ipCachos.length!=4) {
+			System.out.println("Direcion ip DNS in correcta");
+			System.exit(1);
 		}
+		for(int i =0;i<4;i++) {
+			
+			byte b = (byte) Integer.parseInt(ipCachos[i]);
+			 ip[i] = (byte) (b & 0xFF);
+			
+		
+		}
+		Inet4Address ServerPregunta = (Inet4Address) InetAddress.getByAddress(ip);
 		
 		BufferedReader algo = new BufferedReader(new InputStreamReader(System.in));
-		String dominio=algo.readLine();
-		
-		do {
-			
-		System.out.println(dominio);
-		
-		
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		}while((dominio=algo.readLine())!= null);
-		
-		
-	
-		
+		String entrada;
+
+		while ((entrada = algo.readLine()) != null) {
+
+			String partes[] = entrada.split(" ");
+			if (partes.length != 2) {
+				System.out.println("faltan parametros en la entrada");
+				continue;
+			}
+			partes[0] = partes[0].toUpperCase().trim();
+			if ((partes[0].equals("A") || partes[0].equals("NS"))) {
+
+				Message inicial = new Message(partes[1], RRType.valueOf(partes[0]), false);
+				int o = 0;
+				ArrayList<String> dnsConsultados = new ArrayList<>();
+				while (o < 10) {
+
+					System.out.println("Q " + modoConexion + " "
+							+ ServerPregunta.toString().substring(1, ServerPregunta.toString().length()) + " A "
+							+ partes[1]);
+					Message respuesta;
+					do {
+						respuesta = EnvioPaquetes.envioUDP(inicial, ServerPregunta);
+					} while (respuesta == null);
+					// System.out.println(respuesta.getQuestion());
+				//	System.out.println(respuesta.getAnswers().get(0).getRRType());
+					if (!(respuesta.getAnswers().isEmpty())) {
+						System.out.println(respuesta.getAnswers().size());
+						if ((respuesta.getAnswers().get(0)) instanceof AResourceRecord) {
+							System.out.println(((AResourceRecord) (respuesta.getAnswers().get(0))).getAddress());
+						}
+						if ((respuesta.getAnswers().get(0)) instanceof NSResourceRecord) {
+							System.out.println(((NSResourceRecord) (respuesta.getAnswers().get(0))).getNS());
+						}
+						if(respuesta.getAnswers().get(0).getRRType().equals(RRType.CNAME)) {
+							System.out.println("CNAME");
+						}
+						break;
+					} else if ((!respuesta.getNameServers().isEmpty() && !respuesta.getAdditonalRecords().isEmpty())) {
+						boolean a = false;
+						
+						  for(int b=0;b<respuesta.getNameServers().size();b++) {
+						  System.out.println(((NSResourceRecord)
+						  (respuesta.getNameServers().get(b))).getNS()); }
+						 
+						for (int i = 0; i < respuesta.getAdditonalRecords().size(); i++) {
+							if (((respuesta.getAdditonalRecords().get(i))) instanceof AResourceRecord) {
+								if (!dnsConsultados
+										.contains(((AResourceRecord) (respuesta.getAdditonalRecords().get(i)))
+												.getAddress().toString())) {
+
+									ServerPregunta = ((AResourceRecord) (respuesta.getAdditonalRecords().get(i)))
+											.getAddress();
+									dnsConsultados.add(ServerPregunta.toString());
+									System.out.println(
+											((AResourceRecord) (respuesta.getAdditonalRecords().get(i))).getAddress());
+									a = true;
+									break;
+								}
+
+							}
+						}
+						if (a == false) {
+							System.out.println("ni puta");
+							System.exit(1);
+						}
+					} else {
+						System.out.println("no hay respuesta");
+						System.exit(1);
+					}
+					o++;
+				}
+
+				// System.out.println(((AResourceRecord)(respuesta.getAdditonalRecords().get(0))).getAddress().toString());
+
+			} else {
+				System.out.println("type no admitido");
+				continue;
+			}
+
+		}
+
 	}
-	
 
 }
--- src/funciones/EnvioPaquetes.java
+++ src/funciones/EnvioPaquetes.java
@@ -1,5 +1,85 @@
 package funciones;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import es.uvigo.det.ro.simpledns.*;
 public class EnvioPaquetes {
 
+	
+	
+	
+	public static Message envioUDP(Message consulta,Inet4Address direccion_ip)  {
+		
+					   int puerto = 53;
+						
+						
+						DatagramSocket socketUDP;
+						Message respuesta=null;
+						try {
+							socketUDP = new DatagramSocket();
+						
+						socketUDP.setSoTimeout(35000);
+						//pasamos la cadena a bytes
+						byte[] mensaje = consulta.toByteArray();
+					//	System.out.println(mensaje.length);
+						//sacamos la ip del nombre del servidor
+						InetAddress servidor = direccion_ip;
+
+						//creamos el datagrama con los elemntos introducidos
+						DatagramPacket peticionServidor = new DatagramPacket(mensaje, mensaje.length, servidor, puerto);
+
+						//enviamos el datagrama con los datos introducidos
+						socketUDP.send(peticionServidor);
+
+						//creamos el datagrama para la respuesta del servidor
+						byte[] almacen = new byte[1000];
+						
+						DatagramPacket respuestaServidor = new DatagramPacket(almacen, almacen.length);
+						
+						try {
+							//recivimos el paquete, en caso de que el tiempo de espera exceda el time out sacamos excepcion y cerramos socket
+							socketUDP.receive(respuestaServidor);
+
+							
+							 respuesta = new Message(respuestaServidor.getData());
+					
+						 
+						} catch (SocketTimeoutException e) {
+						//	System.out.println("Tiempo de espera excedido,compruebe los datos de conexión");
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						// Cerramos el socket
+						socketUDP.close();
+						
+						} catch (SocketException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						return respuesta;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
